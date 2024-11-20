cd320a558b963fd5d3dda569fc116cb96c8319ad
Edess <Edess@10.3.62.83>
2024-11-20 14:55:19 +0100

    Mini_projet création

---
+++ Mini_projet_CARRERA_PINCEDE/src/mini_projet_carrera_pincede/Mini_projet_CARRERA_PINCEDE.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mini_projet_carrera_pincede;
+
+/**
+ *
+ * @author Edess
+ */
+public class Mini_projet_CARRERA_PINCEDE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
