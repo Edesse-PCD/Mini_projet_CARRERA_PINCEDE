86793c6e8b4a9764d08476d9ff5f82d21c7f8499
Valentin CARRERA <valentin.carrera@epfedu.fr>
2024-12-02 16:40:22 +0100

    Merge origin/master
    Conflicts:
    	Mini_projet_CARRERA_PINCEDE/src/mini_projet_carrera_pincede/GrilleDeJeu.java

--- Mini_projet_CARRERA_PINCEDE/src/mini_projet_carrera_pincede/Mini_projet_CARRERA_PINCEDE.java
+++ Mini_projet_CARRERA_PINCEDE/src/mini_projet_carrera_pincede/Mini_projet_CARRERA_PINCEDE.java
@@ -14,7 +14,5 @@ public class Mini_projet_CARRERA_PINCEDE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        if (!MatriceCellules[1][1].revelerCellule()){
-        }
     }
 }
