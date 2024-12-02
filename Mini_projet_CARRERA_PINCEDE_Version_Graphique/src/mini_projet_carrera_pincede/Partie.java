a41f0b97516e1350416e59d7ae7522b957d30f96
Valentin CARRERA <valentin.carrera@epfedu.fr>
2024-12-02 16:17:37 +0100

    Merge origin/master
    Conflicts:
    	Mini_projet_CARRERA_PINCEDE/src/mini_projet_carrera_pincede/Cellule.java
    	Mini_projet_CARRERA_PINCEDE/src/mini_projet_carrera_pincede/GrilleDeJeu.java

---
+++ Mini_projet_CARRERA_PINCEDE_Version_Graphique/src/mini_projet_carrera_pincede/Partie.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mini_projet_carrera_pincede;
+
+
+/**
+ *
+ * @author Edess
+ */
+public class Partie {
+    GrilleDeJeu grille ;
+    public void initialiserPartie(){
+      GrilleDeJeu  grille = new GrilleDeJeu(10,10,2);
+      grille.placerBombesAleatoirement();
+      
+    }
+    
+}
