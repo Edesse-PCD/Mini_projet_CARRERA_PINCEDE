709447584b3063fab0aa1f3fb4e4a51ed98e79c1
Valentin CARRERA <valentin.carrera@epfedu.fr>
2024-12-13 10:35:04 +0100

    Update graph séance du 13/12

---
+++ Mini_projet_CARRERA_PINCEDE_Version_Graphique/src/mini_projet_carrera_pincede/MatriceCellules.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mini_projet_carrera_pincede;
+
+/**
+ *
+ * @author valen
+ */
+public class MatriceCellules {
+    private Cellule[][] grilleCellules;
+    int nbLignes;
+    int nbColonnes;
+
+    public MatriceCellules(int nbLignes, int nbColonnes) {
+        this.nbLignes = nbLignes;
+        this.nbColonnes = nbColonnes;
+        this.grilleCellules = new Cellule[nbLignes][nbColonnes];
+
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                grilleCellules[i][j] = new Cellule();
+            }
+        }
+    }
+    
+    public void afficherMatrice() {
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                System.out.print(grilleCellules[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
