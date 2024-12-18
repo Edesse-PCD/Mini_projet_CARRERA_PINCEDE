b47ed733fd6bbfcc30c48e485831864b2454a7c0
Edess <Edess@10.3.62.83>
2024-12-18 11:59:02 +0100

    Jeu terminé + amélioration : on propose au joueur de relancer une partie quand il a perdu Je commit car ça ne marche pas pour Val

--- Mini_projet_CARRERA_PINCEDE/src/mini_projet_carrera_pincede/GrilleDeJeu.java
+++ Mini_projet_CARRERA_PINCEDE/src/mini_projet_carrera_pincede/GrilleDeJeu.java
@@ -60,10 +60,31 @@ for (int j=0;j<nbColonnes;j++){
     public int getNbBombes() {
         return nbBombes;
     }
+    
+    
+    
+    public void calculerBombesAdjacentes() {
+    for (int i = 0; i < nbLignes; i++) {  // Remplacer -2 par nbLignes pour inclure toutes les lignes
+        for (int j = 0; j < nbColonnes; j++) {  // Remplacer -2 par nbColonnes pour inclure toutes les colonnes
+            if (!matriceCellules[i][j].isPresenceBombe()) {  // Vérifier si la cellule n'est pas une bombe
+                if (i > 0 && j > 0 && matriceCellules[i-1][j-1].isPresenceBombe()) matriceCellules[i][j].IncrementerNbBombesAdj(i,j);
+                if (i > 0 && matriceCellules[i-1][j].isPresenceBombe()) matriceCellules[i][j].IncrementerNbBombesAdj(i,j);
+                if (i > 0 && j < nbColonnes - 1 && matriceCellules[i-1][j+1].isPresenceBombe()) matriceCellules[i][j].IncrementerNbBombesAdj(i,j);
+                if (j > 0 && matriceCellules[i][j-1].isPresenceBombe()) matriceCellules[i][j].IncrementerNbBombesAdj(i,j);
+                if (j < nbColonnes - 1 && matriceCellules[i][j+1].isPresenceBombe()) matriceCellules[i][j].IncrementerNbBombesAdj(i,j);
+                if (i < nbLignes - 1 && j > 0 && matriceCellules[i+1][j-1].isPresenceBombe()) matriceCellules[i][j].IncrementerNbBombesAdj(i,j);
+                if (i < nbLignes - 1 && matriceCellules[i+1][j].isPresenceBombe()) matriceCellules[i][j].IncrementerNbBombesAdj(i,j);
+                if (i < nbLignes - 1 && j < nbColonnes - 1 && matriceCellules[i+1][j+1].isPresenceBombe()) matriceCellules[i][j].IncrementerNbBombesAdj(i,j);
+                
+            }
+        }
+    }
+    }
+    
 public void placerBombesAleatoirement(int nbBombes){
     Random random = new Random();
  
-     for (int k =1; k <=nbBombes;k++){
+     for (int k =0; k <nbBombes;k++){
 
     int i = random.nextInt(nbLignes);
     int  j = random.nextInt(nbColonnes);
@@ -73,46 +94,35 @@ public void placerBombesAleatoirement(int nbBombes){
     }
    matriceCellules[i][j].placerBombe();
     }
-}
-public void calculerBombesAdjacentes() {
-    for (int i =0;i<nbLignes-2;i++){
-        for (int j=0;j<nbColonnes-2;j++){
-    if (!matriceCellules[i][j].isPresenceBombe()){
-        if (matriceCellules[i-1][j-1].isPresenceBombe())   matriceCellules[i][j].IncrementerNbBombesAdj(i,j);  
-        if (matriceCellules[i-1][j].isPresenceBombe())   matriceCellules[i][j].IncrementerNbBombesAdj(i,j);
-        if (matriceCellules[i-1][j+1].isPresenceBombe())   matriceCellules[i][j].IncrementerNbBombesAdj(i,j);
-        if (matriceCellules[i][j-1].isPresenceBombe())   matriceCellules[i][j].IncrementerNbBombesAdj(i,j);
-        if (matriceCellules[i][j+1].isPresenceBombe())   matriceCellules[i][j].IncrementerNbBombesAdj(i,j);
-        if (matriceCellules[i+1][j-1].isPresenceBombe())   matriceCellules[i][j].IncrementerNbBombesAdj(i,j);
-        if (matriceCellules[i+1][j].isPresenceBombe())   matriceCellules[i][j].IncrementerNbBombesAdj(i,j);
-        if (matriceCellules[i+1][j+1].isPresenceBombe())   matriceCellules[i][j].IncrementerNbBombesAdj(i,j);
-    }
-}
-    }
+
 
 }
-public void revelerCellule(int ligne, int colonne){
-    for ( int i = ligne-1;i<=ligne+1;i++){
-        for (int j = colonne-1;j<=ligne+1;j++){
-            if (!matriceCellules[i][j].isPresenceBombe() && matriceCellules[i][j].getNbBombesAdjacentes()==0){
-        matriceCellules[i][j].revelerCellule();
+public void revelerCellule(int ligne, int colonne) {
+    for (int i = ligne - 1; i <= ligne + 1; i++) {
+        for (int j = colonne - 1; j <= colonne + 1; j++) {
+            // Vérification pour s'assurer que i et j sont dans les limites de la matrice
+            if (i >= 0 && i < nbLignes && j >= 0 && j < nbColonnes) {
+                if (!matriceCellules[i][j].isPresenceBombe() && matriceCellules[i][j].getNbBombesAdjacentes() == 0) {
+                    matriceCellules[i][j].revelerCellule();
+                }
+            }
         }
     }
-        if (!cellAtCoord(ligne,colonne).revelerCellule() && cellAtCoord(ligne,colonne).isPresenceBombe()){
-        System.out.println("Erreur");
-    } else { 
-            cellAtCoord(ligne,colonne).revelerCellule();
- 
-    
-        
+
+    // Révéler la cellule cible si elle n'est pas une bombe
+    if (!cellAtCoord(ligne, colonne).revelerCellule() && cellAtCoord(ligne, colonne).isPresenceBombe()) {
+        System.out.println("Erreur : Bombe détectée !");
+    } else {
+        cellAtCoord(ligne, colonne).revelerCellule();
     }
+}
+
     
     
     
     
 
-        }
-}
+       
 
 public boolean getPresenceBombe(int i, int j) {
 if (cellAtCoord(i,j).isPresenceBombe()) return(true);
@@ -120,43 +130,41 @@ else return(false);
 }
 
 
-public boolean toutesCellulesRevelees(){
-    boolean gagner = true;
-    for(int i =0; i<nbLignes;i++){
-        for(int j=0;j<nbColonnes;j++){
-           if (cellAtCoord(i,j).revelerCellule()) gagner = true;
-           else gagner = false;
+public boolean toutesCellulesRevelees() {
+    // Vérifie si toutes les cellules non-bombe sont révélées
+    for (int i = 0; i < nbLignes; i++) {
+        for (int j = 0; j < nbColonnes; j++) {
+            // Si une cellule non-bombe n'est pas révélée, retourne false
+            if (!cellAtCoord(i, j).isPresenceBombe() && !cellAtCoord(i, j).estDevoilee()) {
+                return false;
+            }
         }
     }
-           return(gagner);
+
+    return true;
 }
 
-    @Override
-    public String toString() {
-        for (int i =0;i<=nbLignes;i++){
-            System.out.println("\n |");
-           for (int j =0;j<=nbLignes;j++){
-               System.out.println(cellAtCoord(i,j).toString());
-               
-           }
+
+ 
+
+    
+
+   @Override
+public String toString() {
+    for (int i = 0; i < nbLignes; i++) {
+        System.out.print("|");  // Ajout du premier séparateur avant chaque ligne
+        for (int j = 0; j < nbColonnes; j++) {  // Utilisation de nbColonnes, pas nbLignes
+            System.out.print(cellAtCoord(i, j).toString() + " | ");  // Affichage du contenu de chaque cellule avec un séparateur
         }
-        return("");
-        
+        System.out.println();  // Ajoute une nouvelle ligne après chaque ligne de la grille
     }
+    return "";  // Retourne une chaîne vide si nécessaire
 }
-       
+
+}
+
     
 
   
         
-
-        
-    
-     
-
-    
-          
-        
-    
-
 
