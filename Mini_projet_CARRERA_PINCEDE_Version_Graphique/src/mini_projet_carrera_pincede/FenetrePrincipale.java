edd9cce2170a1d84324b14768cac4b05259acc84
Valentin CARRERA <valentin.carrera@epfedu.fr>
2024-12-18 17:53:09 +0100

    problèmes dans fenetre principale, commit pour push les modifs d'edesse

--- Mini_projet_CARRERA_PINCEDE_Version_Graphique/src/mini_projet_carrera_pincede/FenetrePrincipale.java
+++ Mini_projet_CARRERA_PINCEDE_Version_Graphique/src/mini_projet_carrera_pincede/FenetrePrincipale.java
@@ -1,116 +1,62 @@
 package mini_projet_carrera_pincede;
- 
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.GridLayout;
-import javax.swing.JPanel;
+import java.util.Scanner;
+import javax.swing.JButton;
+import mini_projet_carrera_pincede.Partie;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 
-/**
- *
- * @author Valentin
- */
-
-/**
- * Classe FenetrePrincipale pour afficher la grille du jeu.
- * 
- * @author Valentin
- */
 public final class FenetrePrincipale extends javax.swing.JFrame {
-    public GrilleDeJeu grilleDeJeu;
-    public JPanel panneauGrille;
+    private GrilleDeJeu grilleDeJeu;
+    private final Scanner sc = new Scanner (System.in);
     
     public FenetrePrincipale() {
         initComponents();
-        this.grilleDeJeu = new GrilleDeJeu(10, 10, 15);
-        grilleDeJeu.placerBombesAleatoirement(15);
-        grilleDeJeu.calculerBombesAdjacentes(); 
-    }
-
-    public void getMatriceCellules() {
-        panneauGrille.setLayout(new GridLayout(10,10));
+        int nbLignes = 0;
+        int nbColonnes = 0;
+        int nbBombes = 0;
+        int nbVie = 0;
+        this.grilleDeJeu = new GrilleDeJeu(nbLignes, nbColonnes, nbBombes);
         
-        for (int i = 0; i < grilleDeJeu.getNbLignes(); i++) {
-            for (int j = 0; j < grilleDeJeu.getNbColonnes(); j++) {
-                getMatriceCellules = grilleDeJeu.cellAtCoord(i, j);  
-                panneauGrille.add(getMatriceCellules);
-            }
-        }
-        this.add(panneauGrille);
-        this.pack();
-        this.setVisible(true);
+        initialiserPartie();
     }
+    
+    
 
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        PanneauGrille = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        PanneauGrille.setBackground(new java.awt.Color(51, 255, 102));
+        jPanel1.setBackground(new java.awt.Color(255, 255, 0));
 
-        javax.swing.GroupLayout PanneauGrilleLayout = new javax.swing.GroupLayout(PanneauGrille);
-        PanneauGrille.setLayout(PanneauGrilleLayout);
-        PanneauGrilleLayout.setHorizontalGroup(
-            PanneauGrilleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 480, Short.MAX_VALUE)
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 490, Short.MAX_VALUE)
         );
-        PanneauGrilleLayout.setVerticalGroup(
-            PanneauGrilleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 450, Short.MAX_VALUE)
         );
 
-        getContentPane().add(PanneauGrille, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 30, 480, 450));
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 30, 490, 450));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FenetrePrincipale.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FenetrePrincipale.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FenetrePrincipale.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FenetrePrincipale.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new FenetrePrincipale().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel PanneauGrille;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
