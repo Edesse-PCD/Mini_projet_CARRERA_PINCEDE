bbbfa6113b2fe268cf499d0cce66b33dc8d30122
Edess <Edess@10.3.62.83>
2024-12-18 12:12:54 +0100

    J'ai pull la version de Valentin mais maintenant le code bug

---
+++ Mini_projet_CARRERA_PINCEDE_Version_Graphique/src/mini_projet_carrera_pincede/Mini_projet_CARRERA_PINCEDE.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mini_projet_carrera_pincede;
+
+
+/**
+ *
+ * @author Edess
+ */
+public class Mini_projet_CARRERA_PINCEDE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Partie partie = new Partie(); // Appelle le constructeur par défaut
+        partie.demarrerPartie();    
+    
+    
+    }
+        
+    
+
+            
+        
+
+  
+    }
+
+
+    
+    
+    
+
+    
